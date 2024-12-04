c7895ce78e322de7acb20bd21d136a16a265b309
David Henrique <dh210605@gmail.com>
2024-12-03 23:22:47 -0300

    fix: cors config

--- src/main/java/com/example/BancoDeDados/Controller/ControllerEstudante.java
+++ src/main/java/com/example/BancoDeDados/Controller/ControllerEstudante.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.support.Repositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/estudantes")
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class ControllerEstudante {
 
     @Autowired
--- src/main/java/com/example/BancoDeDados/Security/WebConfig.java
+++ src/main/java/com/example/BancoDeDados/Security/WebConfig.java
@@ -5,10 +5,14 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("*");
+        registry.addMapping("/**") 
+                .allowedOrigins("http://localhost:3000") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
+                .allowedHeaders("*") 
+                .allowCredentials(true); 
     }
 }
-
